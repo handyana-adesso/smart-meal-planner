5b9dce874bf541816dbc911e22d96cfa682f69c4
Handyana S. Atmaja <handyana-sumitra.atmaja@adesso.de>
2026-05-28 16:47:03 +0200

    remove weather endpoint and add health check endpoint

--- backend-dotnet-recipe-budget/backend-dotnet-recipe-budget/Program.cs
+++ backend-dotnet-recipe-budget/backend-dotnet-recipe-budget/Program.cs
@@ -4,6 +4,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+// Add health checks
+builder.Services.AddHealthChecks();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -14,24 +17,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+// Map health check endpoint
+app.MapHealthChecks("/health");
 
 app.Run();
 
