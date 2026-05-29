65560dbe48540ae2116ae57097cd6bd31acf4054
Handyana S. Atmaja <handyana-sumitra.atmaja@adesso.de>
2026-05-29 19:59:49 +0200

    Add recipe service

---
+++ services/backend-dotnet-recipe-budget/RecipeBudgetService/Services/IRecipeService.cs
@@ -0,0 +1,12 @@
+﻿using RecipeBudgetService.DTOs;
+
+namespace RecipeBudgetService.Services;
+
+public interface IRecipeService
+{
+    Task<IEnumerable<RecipeResponse>> GetAllAsync(CancellationToken cancellationToken);
+    Task<RecipeResponse?> GetByIdAsync(Guid id, CancellationToken cancellationToken);
+    Task<RecipeResponse> CreateAsync(RecipeRequest request, CancellationToken cancellationToken);
+    Task<RecipeResponse?> UpdateAsync(Guid id, RecipeRequest request, CancellationToken cancellationToken);
+    Task<bool> DeleteAsync(Guid id, CancellationToken cancellationToken);
+}
---
+++ services/backend-dotnet-recipe-budget/RecipeBudgetService/Services/RecipeService.cs
@@ -0,0 +1,46 @@
+﻿using RecipeBudgetService.Common.Extensions;
+using RecipeBudgetService.Common.Mappers;
+using RecipeBudgetService.DTOs;
+using RecipeBudgetService.Repositories;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("RecipeBudgetService.Tests")]
+
+namespace RecipeBudgetService.Services;
+
+internal class RecipeService(IRecipeRepository repository) : IRecipeService
+{
+    private readonly IRecipeRepository _repository = repository
+        ?? throw new ArgumentNullException(nameof(repository));
+
+    public async Task<RecipeResponse> CreateAsync(RecipeRequest request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> DeleteAsync(Guid id, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<IEnumerable<RecipeResponse>> GetAllAsync(CancellationToken cancellationToken)
+    {
+        var recipes = await _repository.GetAllAsync(cancellationToken);
+        return recipes is null
+            ? Enumerable.Empty<RecipeResponse>()
+            : recipes.Select(r => r.ToResponse());
+    }
+
+    public async Task<RecipeResponse?> GetByIdAsync(Guid id, CancellationToken cancellationToken)
+    {
+        GuardExtensions.ThrowIfGuidEmpty(id);
+
+        var recipe = await _repository.GetByIdAsync(id, cancellationToken);
+        return recipe?.ToResponse();
+    }
+
+    public Task<RecipeResponse?> UpdateAsync(Guid id, RecipeRequest request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
