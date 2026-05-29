5ed8aed1de33016d6b2219b757c452ae1cc6bc4c
Handyana S. Atmaja <handyana-sumitra.atmaja@adesso.de>
2026-05-29 23:39:30 +0200

    Add a new method to check decimal negative

--- services/backend-dotnet-recipe-budget/RecipeBudgetService/Common/Extensions/GuardExtensions.cs
+++ services/backend-dotnet-recipe-budget/RecipeBudgetService/Common/Extensions/GuardExtensions.cs
@@ -13,4 +13,14 @@ public static class GuardExtensions
             throw new ArgumentException("Guid cannot be empty", paramName);
         }
     }
+
+    public static void ThrowIfDecimalNegative(
+        decimal value,
+        [CallerArgumentExpression(nameof(value))] string? paramName = null)
+    {
+        if (value < 0)
+        {
+            throw new ArgumentException("Decimal value cannot be negative", paramName);
+        }
+    }
 }
