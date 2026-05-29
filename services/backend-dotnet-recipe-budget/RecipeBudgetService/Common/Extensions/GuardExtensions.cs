c1467b2ac57e94bfc75c5cfba2cc5007b40fff50
Handyana S. Atmaja <handyana-sumitra.atmaja@adesso.de>
2026-05-29 19:59:33 +0200

    Add static guard class

---
+++ services/backend-dotnet-recipe-budget/RecipeBudgetService/Common/Extensions/GuardExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace RecipeBudgetService.Common.Extensions;
+
+public static class GuardExtensions
+{
+    public static void ThrowIfGuidEmpty(
+        Guid value,
+        [CallerArgumentExpression(nameof(value))] string? paramName = null)
+    {
+        if (value == Guid.Empty)
+        {
+            throw new ArgumentException("Guid cannot be empty", paramName);
+        }
+    }
+}
