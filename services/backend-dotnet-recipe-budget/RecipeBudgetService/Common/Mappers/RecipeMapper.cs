2aa8c0e5d9d93f830d49b7d83b5fe2fe8efafd8b
Handyana S. Atmaja <handyana-sumitra.atmaja@adesso.de>
2026-05-29 19:59:01 +0200

    Add recipe mapper

---
+++ services/backend-dotnet-recipe-budget/RecipeBudgetService/Common/Mappers/RecipeMapper.cs
@@ -0,0 +1,20 @@
+﻿using RecipeBudgetService.DTOs;
+using RecipeBudgetService.Entities;
+
+namespace RecipeBudgetService.Common.Mappers;
+
+public static class RecipeMapper
+{
+    public static RecipeResponse ToResponse(this Recipe recipe) =>
+        new(recipe.Id, recipe.Name, recipe.Description, recipe.Servings, recipe.EstimatedCost);
+
+    public static Recipe ToEntity(this RecipeResponse response, Guid? id = null) =>
+        new() 
+        { 
+            Id = id ?? Guid.Empty, 
+            Name = response.Name, 
+            Description = response.Description, 
+            Servings = response.Servings, 
+            EstimatedCost = response.EstimatedCost 
+        };
+}
