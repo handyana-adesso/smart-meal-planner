cbbd43247d960b6988b67f022601227649db0901
Handyana S. Atmaja <handyana-sumitra.atmaja@adesso.de>
2026-05-31 00:29:00 +0200

    Add service and repository interface for ingredient

---
+++ services/backend-dotnet-recipe-budget/RecipeBudgetService/Repositories/IIngredientRepository.cs
@@ -0,0 +1,9 @@
+﻿using RecipeBudgetService.Entities;
+
+namespace RecipeBudgetService.Repositories;
+
+public interface IIngredientRepository
+{
+    Task<Ingredient> CreateAsync(Ingredient ingredient, CancellationToken cancellationToken);
+    Task<bool> DeleteAsync(Guid id, CancellationToken cancellationToken);
+}
---
+++ services/backend-dotnet-recipe-budget/RecipeBudgetService/Services/IIngredientService.cs
@@ -0,0 +1,9 @@
+﻿using RecipeBudgetService.DTOs;
+
+namespace RecipeBudgetService.Services;
+
+public interface IIngredientService
+{
+    Task<IngredientResponse> CreateAsync(Guid recipeId,IngredientRequest request, CancellationToken cancellationToken);
+    Task DeleteAsync(Guid recipeId, Guid ingredientId, CancellationToken cancellationToken);
+}
