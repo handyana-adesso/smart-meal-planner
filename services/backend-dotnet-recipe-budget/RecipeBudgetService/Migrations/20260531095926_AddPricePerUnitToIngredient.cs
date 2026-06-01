eb37bc867e968f7a408dfb8021177d6520ed18f3
Handyana S. Atmaja <handyana-sumitra.atmaja@adesso.de>
2026-06-01 12:31:40 +0200

    Migrations script to add estimatedcosts in ingredient

---
+++ services/backend-dotnet-recipe-budget/RecipeBudgetService/Migrations/20260531095926_AddPricePerUnitToIngredient.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RecipeBudgetService.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPricePerUnitToIngredient : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EstimatedCost",
+                table: "Recipes");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Recipes",
+                type: "character varying(1000)",
+                maxLength: 1000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(500)",
+                oldMaxLength: 500);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "PricePerUnit",
+                table: "Ingredients",
+                type: "numeric(18,2)",
+                precision: 18,
+                scale: 2,
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PricePerUnit",
+                table: "Ingredients");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Recipes",
+                type: "character varying(500)",
+                maxLength: 500,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(1000)",
+                oldMaxLength: 1000);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "EstimatedCost",
+                table: "Recipes",
+                type: "numeric(18,2)",
+                precision: 18,
+                scale: 2,
+                nullable: false,
+                defaultValue: 0m);
+        }
+    }
+}
